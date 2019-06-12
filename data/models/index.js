f0d4f7d9e09975da200329c00c2d7704b3fdde1e
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-12 15:05:49 +0800

    Add data models

---
+++ data/models/index.js
@@ -0,0 +1,102 @@
+// Initiate database connection
+const db = require('../dbConfig')
+
+// ==== Application Methods ==== //
+async function getUsers() {
+  try {
+    let data = await db('Users')
+      .select('id', 'username')
+      .orderBy('id')
+    return data
+  }
+  catch (err) {
+    return err
+  }
+}
+
+// ==== Global Database Methods ==== //
+async function find(table) {
+  try {
+    let data = await db(table).orderBy('id')
+    return data
+  }
+  catch (err) {
+    return err
+  }
+}
+
+async function findById(id, table) {
+  try {
+    let data = await db(table)
+      .where({ id: Number(id) })
+      .first()
+    return data
+  }
+  catch (err) {
+    return err
+  }
+}
+
+async function findByUser(username, table) {
+  try {
+    let data = await db(table)
+      .where({ username })
+      .first()
+    return data
+  }
+  catch (err) {
+    return err
+  }
+}
+
+async function insert(data, table) {
+  try {
+    let newRecordId = await db(table).insert(data, 'id')
+    let newRecord = await findById(newRecordId, 'Users')
+    return newRecord
+  }
+  catch (err) {
+    return err
+  }
+}
+
+async function update(id, data, table) {
+  try {
+    let updateCount = await db(table)
+      .where({ id }).update(data)
+    if (updateCount > 0) {
+      let updatedRecord = await findById(id)
+      return updatedRecord
+    } else throw err
+  }
+  catch (err) {
+    return err
+  }
+}
+
+async function remove(id, table) {
+  try {
+    let deleteCount = await db(table)
+      .where({ id }).del()
+    if (deleteCount > 0) {
+      return {
+        message: `${deleteCount} ${deleteCount > 1 ?
+          'records' : 'record'} deleted`
+      }
+    } else throw err
+  }
+  catch (err) {
+    return err
+  }
+}
+
+module.exports = {
+  db,
+  find,
+  findById,
+  findByUser,
+  insert,
+  update,
+  remove,
+  getUsers
+}

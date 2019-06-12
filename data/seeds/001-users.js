d6053caef154772c828811df159b0f0da25afab5
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-12 19:03:26 +0800

    Add password hashing to test data

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,8 +1,9 @@
+const bcrypt = require('bcryptjs')
 
 exports.seed = async function(knex) {
   await knex('Users').insert([
-    { id: 1, username: 'George', password: 'pass', department: 'CS' },
-    { id: 2, username: 'Steve', password: 'pass', department: 'Biology' },
-    { id: 3, username: 'Harry', password: 'abc123', department: 'Accounting' }
+    { id: 1, username: 'George', password: bcrypt.hashSync('pass', 12), department: 'CS' },
+    { id: 2, username: 'Steve', password: bcrypt.hashSync('pass', 12), department: 'Biology' },
+    { id: 3, username: 'Harry', password: bcrypt.hashSync('abc123', 12), department: 'Accounting' }
   ])
 }

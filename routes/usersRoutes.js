00460c838ac9939da7460c954d561a3ee41cf221
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-13 23:36:57 +0800

    Add PUT and DELETE routes to Users resource

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,16 +4,43 @@ const router = require('express').Router()
 const db = require('../data/models')
 
 // Middleware
-const { inputDataChecker, requiredData, validateData, userAuthorization } = require('../middleware')
+const { inputDataChecker, requiredData, validateData } = require('../middleware')
+const updateUserData = ['username', 'department']
+const IdDataCheck = [requiredData(inputDataChecker, updateUserData), validateData('Users')]
 
 // User Resource Routes
 //==== GET ====//
-router.get('/', userAuthorization, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     let data = await db.getUsers()
     res.send(data)
   }
   catch(err) {
+    res.status(500).send(err.message)
+  }
+})
+
+//==== PUT ====//
+router.put('/:id', IdDataCheck, async (req, res) => {
+  try {
+    let data = await db.update(req.data.id, req.body, 'Users')
+    res.send(data)
+  }
+  catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+//==== DELETE ====//
+router.delete('/:id', validateData('Users'), async (req, res) => {
+  try {
+    let data = await db.remove(req.data.id)
+    if (data <= 0) throw err
+    else {
+      res.json({ message: `Successfully deleted record ${req.params.id}` })
+    }
+  }
+  catch (err) {
     res.status(500).send(err.message)
   }
 })
